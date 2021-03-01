eccdcf4086460bd005a702f63d881165294c50e3
Grigory Osipenkov <grigory.osipenkov@gmail.com>
2021-03-02 03:15:18 +1000

    editorconfig

--- spacex_dag.py
+++ spacex_dag.py
@@ -17,14 +17,14 @@ default_args = {
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
 t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
+    task_id="get_data",
+    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data",
     dag=dag
 )
 
 t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+    task_id="print_data",
+    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv",
     params={"rocket": "all"}, # falcon1/falcon9/falconheavy
     dag=dag
 )
