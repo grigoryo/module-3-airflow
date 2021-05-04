27f1ed45a9892bc87e5ad1d7dfe797480db7189a
Grigory Osipenkov <grigory.osipenkov@rtlabs.ru>
2021-05-04 18:33:48 +1000

    dwh: VARCHAR changed to TEXT

--- dwh/etl/02-ods_v_payment.sql
+++ dwh/etl/02-ods_v_payment.sql
@@ -18,44 +18,44 @@ SELECT
     -- <https://dbtvault.readthedocs.io/en/latest/best_practices/#how-do-we-hash>
 
     CAST(MD5(NULLIF(CONCAT_WS('||',
-        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_type AS VARCHAR))), ''), '^^'),
-        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_num AS VARCHAR))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_type AS TEXT))), ''), '^^'),
+        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_num AS TEXT))), ''), '^^')
     ), '^^||^^')) AS TEXT) AS pay_doc_pk,
 
     CAST(MD5(NULLIF(CONCAT_WS('||',
-        COALESCE(NULLIF(UPPER(TRIM(CAST(user_id AS VARCHAR))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(user_id AS TEXT))), ''), '^^')
     ), '^^')) AS TEXT) AS user_pk,
 
     CAST(MD5(NULLIF(CONCAT_WS('||',
-        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_type AS VARCHAR))), ''), '^^'),
-        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_num AS VARCHAR))), ''), '^^'),
-        COALESCE(NULLIF(UPPER(TRIM(CAST(user_id AS VARCHAR))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_type AS TEXT))), ''), '^^'),
+        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_num AS TEXT))), ''), '^^'),
+        COALESCE(NULLIF(UPPER(TRIM(CAST(user_id AS TEXT))), ''), '^^')
     ), '^^||^^||^^')) AS TEXT) AS payment_pk,
 
     CAST(MD5(CONCAT_WS('||',
         -- hub PK fields
-        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_type AS VARCHAR))), ''), '^^'),
-        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_num AS VARCHAR))), ''), '^^'),
+        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_type AS TEXT))), ''), '^^'),
+        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_num AS TEXT))), ''), '^^'),
         -- payload fields
-        COALESCE(NULLIF(UPPER(TRIM(CAST(pay_date AS VARCHAR))), ''), '^^'),
-        COALESCE(NULLIF(UPPER(TRIM(CAST(amount AS VARCHAR))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(pay_date AS TEXT))), ''), '^^'),
+        COALESCE(NULLIF(UPPER(TRIM(CAST(amount AS TEXT))), ''), '^^')
     )) AS TEXT) AS pay_doc_detail_hashdiff,
 
     CAST(MD5(CONCAT_WS('||',
         -- hub PK fields
-        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_type AS VARCHAR))), ''), '^^')
-        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_num AS VARCHAR))), ''), '^^')
-        COALESCE(NULLIF(UPPER(TRIM(CAST(user_id AS VARCHAR))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_type AS TEXT))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(doc_num AS TEXT))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(user_id AS TEXT))), ''), '^^')
         -- payload fields
-        COALESCE(NULLIF(UPPER(TRIM(CAST(billing_period AS VARCHAR))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(billing_period AS TEXT))), ''), '^^')
     )) AS TEXT) AS payment_billing_period_hashdiff
 
     CAST(MD5(CONCAT_WS('||',
         -- hub PK fields
-        COALESCE(NULLIF(UPPER(TRIM(CAST(user_id AS VARCHAR))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(user_id AS TEXT))), ''), '^^')
         -- payload fields
-        COALESCE(NULLIF(UPPER(TRIM(CAST(account AS VARCHAR))), ''), '^^'),
-        COALESCE(NULLIF(UPPER(TRIM(CAST(phone AS VARCHAR))), ''), '^^')
+        COALESCE(NULLIF(UPPER(TRIM(CAST(account AS TEXT))), ''), '^^'),
+        COALESCE(NULLIF(UPPER(TRIM(CAST(phone AS TEXT))), ''), '^^')
     )) AS TEXT) AS user_detail_hashdiff
 FROM
     gosipenkov.ods_payment;
