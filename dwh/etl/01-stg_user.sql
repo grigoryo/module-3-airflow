820bd2c3ccdb3de2e72593a9c7b9bb584bf04a60
Grigory Osipenkov <grigory.osipenkov@rtlabs.ru>
2021-05-23 23:24:45 +1000

    dwh: added etl for stg_user

---
+++ dwh/etl/01-stg_user.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION gosipenkov.load_stg_user () RETURNS VOID LANGUAGE plpgsql AS $$
+BEGIN
+    TRUNCATE TABLE gosipenkov.stg_user;
+
+    INSERT INTO gosipenkov.stg_user
+    (
+        id,
+        legal_type,
+        district,
+        registered_at,
+        billing_mode,
+        is_vip
+    )
+    SELECT
+        id,
+        legal_type,
+        district,
+        registered_at,
+        billing_mode,
+        is_vip
+    FROM mdm.user;
+
+END;
+$$;
