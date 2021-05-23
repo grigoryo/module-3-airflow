b94c13209397d3e10e15f2312e6f5326999c49be
Grigory Osipenkov <grigory.osipenkov@rtlabs.ru>
2021-05-23 22:44:32 +1000

    dwh: stg_user ddl enhanced

--- dwh/tables/01-stg_user.sql
+++ dwh/tables/01-stg_user.sql
@@ -1,15 +1,15 @@
 CREATE TABLE gosipenkov.stg_user (
-    id INTEGER NOT NULL,
+    id INT,
     legal_type TEXT,
     district TEXT,
-    registered_at TIMESTAMP WITHOUT TIME ZONE,
+    registered_at TIMESTAMP,
     billing_mode TEXT,
-    is_vip BOOLEAN NOT NULL
+    is_vip BOOLEAN
 );
 
 /*
 id      legal_type   district   registered_at         billing_mode   is_vip
-I nn    S            S          TS WO TZ              S              B nn
+I       S            S          DT                    S              B
 ---------------------------------------------------------------------------
 10410   Физлицо      ЦФО        2012-12-26 00:00:00   Предоплатный   TRUE
 10780   Юрлицо       ЦФО        2010-12-23 00:00:00   Предоплатный   FALSE
