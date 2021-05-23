afa237d0ece26e84b5f7c67892dcf80334e15797
Grigory Osipenkov <grigory.osipenkov@rtlabs.ru>
2021-05-24 03:28:40 +1000

    dwh: ods_issue ddl fixed

--- dwh/tables/02-ods_issue.sql
+++ dwh/tables/02-ods_issue.sql
@@ -1,7 +1,7 @@
 CREATE TABLE gosipenkov.ods_issue (
     user_id INT,
-    start_date TIMESTAMP,
-    end_date TIMESTAMP,
+    start_time TIMESTAMP,
+    end_time TIMESTAMP,
     title TEXT,
     description TEXT,
     service TEXT,
